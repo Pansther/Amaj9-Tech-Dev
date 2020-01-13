bf4730631b726ca2167908a773c0c814b7ce126c
Boat <werawit_boat@windowslive.com>
2020-01-13 15:25:02 +0700

    add navbar

---
+++ src/component/navbar/menuData.js
@@ -0,0 +1,22 @@
+export const menuData = [
+  {
+    id: 1,
+    name: 'หน้าแรก',
+    href: "index.html"
+  },
+  {
+    id: 2,
+    name: 'หมวดสินค้า',
+    href: "https://facebook.com"
+  },
+  {
+    id: 3,
+    name: 'ตะกร้า',
+    href: "https://facebook.com"
+  },
+  {
+    id: 4,
+    name: 'เข้าสู่ระบบ',
+    href: "https://facebook.com"
+  }
+];
