15e59c707f7998f4b818fea84072aecf5f32d421
Boat <werawit_boat@windowslive.com>
2020-01-14 16:00:27 +0700

    edit route component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import React from 'react';
-import './App.css'; 
-
-import { Route } from 'react-router-dom'
-import { Switch } from 'react-router-dom'
+import './App.css';
 
 /// components
 import Navbar from './component/navbar/index.js';
-
-/// pages
-import Home from './pages/home/index.js';
-import Category from './pages/category/index.js';
+import MyRoute from './component/route/index.js';
 
 /// css
 import './css/mainStyle.css';
-
-const NotFoundPage =()=> <h1>NotFoundPage</h1>;
 
 function App() {
   return (
     <div className="Main">
       <Navbar />
-
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/home" component={Home} />
-        <Route path="/category" component={Category} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <MyRoute />
     </div>
   );
 }
