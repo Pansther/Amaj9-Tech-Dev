9bdbb54153256673dc86f0370ef0d38bbfdf21a0
Hiinnn <hin_pansa@hotmail.com>
2020-01-22 20:44:12 +0700

    Href

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -6,9 +6,9 @@ export default function Footer() {
             <div className="footer">
                 <div className="store-wrapper">
                     <div style={{ display: 'inline' }}>อีกหลากหลายวิธีในการเลือกซื้อ: ไปที่ </div>
-                    <a href="" style={{ display: 'inline', color: 'darkcyan' }}>Amaj9 Store</a>
+                    <a href="Amaj9Store" style={{ display: 'inline', color: 'darkcyan' }}>Amaj9 Store</a>
                     <div style={{ display: 'inline' }}>, โทร 001‑000‑69‑6969 English support available. หรือ </div>
-                    <a href="" style={{ display: 'inline', color: 'darkcyan' }}>ตัวแทนจำหน่าย</a>
+                    <a href="Dealer" style={{ display: 'inline', color: 'darkcyan' }}>ตัวแทนจำหน่าย</a>
                 </div>
 
                 <div className="hline"></div>
@@ -16,13 +16,13 @@ export default function Footer() {
                 <div className="contact-wrapper">
                     <div className="copyright">Copyright © 6969 B.C. Amaj9 Technologies Inc. สงวนสิทธิ์ทุกประการ</div>
                     <div className="contact-vline">
-                        <a id="contact" href="#">Facebook </a>
+                        <a id="contact" href="Facebook">Facebook </a>
                     </div>
                     
-                    <a id="contact" href="#">Instagram </a>
-                    <a id="contact" href="#">Twitter </a>
-                    <a id="contact" href="#">Line </a>
-                    <a id="contact" href="#">VK</a>
+                    <a id="contact" href="Instagram">Instagram </a>
+                    <a id="contact" href="Twitter">Twitter </a>
+                    <a id="contact" href="Line">Line </a>
+                    <a id="contact" href="VK">VK</a>
                 </div>
 
             </div>
